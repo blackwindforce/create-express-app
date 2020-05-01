dd041291d2b6f53074287fbca5ec17f33035e872
Akira Tsai <blackwindforce@users.noreply.github.com>
2020-05-02 05:48:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const Benchmark = require("benchmark");
 
-const Y = (f) => ((g) => g(g))((g) => f((x) => g(g)(x)));
-const F = (x) => (x === 0 ? 1 : x * F(x - 1));
-const YF = Y(() => F);
+const f = (x) => (x === 0 ? 0 : () => f(x - 1));
+const F = (f) => (x) => (x === 0 ? 0 : () => f(x - 1));
+const Y = (f) => ((f) => f(f))((g) => f((x) => g(g)(x)));
+const T = (f) => (x) => {
+  let y = f(x);
+  while (typeof y === "function") y = y();
+  return y;
+};
+const TF = T(f);
+const TYF = T(Y(F));
 
 new Benchmark.Suite("factorial")
-  .add("F", () => F(100))
-  .add("Y", () => YF(100))
+  .add("F", () => TF(10000))
+  .add("Y", () => TYF(10000))
   .on("cycle", (event) => console.log(`${event.target}`))
   .on("complete", function () {
     console.log(`Fastest is ${this.filter("fastest").map("name")}`);
