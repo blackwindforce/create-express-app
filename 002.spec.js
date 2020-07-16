b27693664ac738719d617fb523f4fae5d0311f2c
Akira Cai <blackwindforce@hotmail.com>
2020-07-16 10:27:39 +0800

    feat: 002

---
+++ 002.spec.js
@@ -0,0 +1,15 @@
+const { none, some } = require("fp-ts/lib/Option");
+
+const f = require("./002");
+
+it("should return none", () => {
+  expect(f(-1)).toStrictEqual(none);
+  expect(f(0)).toStrictEqual(none);
+  expect(f(2)).toStrictEqual(none);
+});
+
+it.skip("should return some", () => {
+  expect(f(1)).toStrictEqual(some("*"));
+  expect(f(3)).toStrictEqual(some(" *\n***\n *"));
+  expect(f(5)).toStrictEqual(some("  *\n ***\n*****\n ***\n  *"));
+});
