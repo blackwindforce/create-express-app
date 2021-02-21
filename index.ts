40726434d497b25730e08b082026ce277128dd4e
Akira Cai <blackwindforce@users.noreply.github.com>
2021-02-21 21:49:39 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,30 +1,50 @@
 import fs from "fs/promises";
 
+//
+
 import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
 import * as RTE from "fp-ts/ReaderTaskEither";
 import * as TE from "fp-ts/TaskEither";
-import { pipe } from "fp-ts/function";
 
-interface Env {
-  fs: typeof fs;
+//
+
+type FsPromises = typeof fs;
+
+interface Context {
+  fs: FsPromises;
 }
+
+//
+
+const readFile = (path: string): RTE.ReaderTaskEither<Context, Error, string> =>
+  pipe(
+    RTE.asks((context: Context) => context.fs),
+    RTE.chainTaskEitherK((fs) =>
+      TE.tryCatch(async () => await fs.readFile(path, "utf8"), E.toError)
+    )
+  );
+
+const parseJson = (a: string): E.Either<Error, E.Json> =>
+  E.parseJSON(a, E.toError);
+
+//
 
 interface PackageJson extends E.JsonRecord {
   name: string;
 }
 
+const getPackage = pipe(readFile("package.json"), RTE.chainEitherK(parseJson));
+
 const getPackageName = pipe(
-  RTE.asks((env: Env) => env.fs),
-  RTE.chainTaskEitherK((fs) =>
-    TE.tryCatch(
-      async () => await fs.readFile("package.json", "utf8"),
-      E.toError
-    )
-  ),
-  RTE.chainEitherK((a) => E.parseJSON(a, E.toError)),
+  getPackage,
   RTE.map((a) => (a as PackageJson).name)
 );
 
-RTE.run(getPackageName, { fs })
+//
+
+const context: Context = { fs };
+
+RTE.run(getPackageName, context)
   .then(E.fold(console.error, console.log))
   .catch(console.error);
