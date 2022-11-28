051f7026f5d714fae00e9a563c25ee4d3b8296b7
Akira Cai <blackwindforce@users.noreply.github.com>
2022-11-29 00:51:03 +0800

    Delete exercism/typescript directory

--- exercism/typescript/hello-world.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-import { hello } from "./hello-world";
-
-describe("Hello World", () => {
-  it("says hello world", () => {
-    expect(hello()).toEqual("Hello, World!");
-  });
-});
--- exercism/typescript/leap.ts
+++
@@ -1,2 +0,0 @@
-export const isLeap = (year: number): boolean =>
-  (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
