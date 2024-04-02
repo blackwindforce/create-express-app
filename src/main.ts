f7cdd18d1e8e914864dfcb3e9f01163577d72bfd
Akira Cai <blackwindforce@users.noreply.github.com>
2024-04-03 05:16:00 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,46 @@
-import * as C from "node:crypto";
-
-//
+import type { UUID } from "node:crypto";
+import { randomInt, randomUUID } from "node:crypto";
 
 import * as E from "fp-ts/lib/Either.js";
+import type { ReaderTaskEither } from "fp-ts/lib/ReaderTaskEither.js";
 import * as RTE from "fp-ts/lib/ReaderTaskEither.js";
 import * as RA from "fp-ts/lib/ReadonlyArray.js";
+import type { StateReaderTaskEither } from "fp-ts/lib/StateReaderTaskEither.js";
 import * as SRTE from "fp-ts/lib/StateReaderTaskEither.js";
 import * as TE from "fp-ts/lib/TaskEither.js";
 import { flow, identity, pipe } from "fp-ts/lib/function.js";
 
-//
-
 type S = Readonly<Record<string, unknown>>;
-type R = Readonly<{ client: typeof client; service: typeof service }>;
+type R = Readonly<{ repository: typeof repository; service: typeof service }>;
 type E = unknown;
-type A = readonly Readonly<{ id: C.UUID; name: string }>[];
+type A = ReadonlyArray<Readonly<{ id: UUID }>>;
 
-//
-
-const client = {
-  raw: () =>
-    C.randomInt(2)
-      ? Promise.resolve(
-          RA.of({ id: C.randomUUID(), name: C.randomBytes(4).toString("hex") })
-        )
+const repository = {
+  select: () =>
+    randomInt(2)
+      ? Promise.resolve(RA.of({ id: randomUUID() }))
       : Promise.reject(new Error()),
 };
 
-//
-
 const service = {
-  findMany: (): RTE.ReaderTaskEither<R, E, A> =>
+  query: (): ReaderTaskEither<R, E, A> =>
     pipe(
       RTE.ask<R>(),
-      RTE.chainTaskEitherK((r) => TE.tryCatch(() => r.client.raw(), identity))
+      RTE.chainTaskEitherK((r) =>
+        TE.tryCatch(() => r.repository.select(), identity),
+      ),
     ),
 };
-
-//
 
 const controller = {
-  list: (): SRTE.StateReaderTaskEither<S, R, E, A> =>
+  list: (): StateReaderTaskEither<S, R, E, A> =>
     pipe(
       SRTE.ask<S, R>(),
-      SRTE.chainReaderTaskEitherK((r) => r.service.findMany()),
-      SRTE.chainFirst((a) => SRTE.modify((s) => ({ ...s, a })))
+      SRTE.chainReaderTaskEitherK((r) => r.service.query()),
+      SRTE.chainFirst((a) => SRTE.modify((s) => ({ ...s, a }))),
     ),
 };
 
-//
+const main = SRTE.execute({})(controller.list())({ repository, service });
 
-const program = SRTE.execute({})(controller.list())({ client, service });
-
-//
-
-await program().then(flow(E.toUnion, console.log));
+await main().then(flow(E.toUnion, console.log));
