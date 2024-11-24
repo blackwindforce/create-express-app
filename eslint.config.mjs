8ba69da136bed7c1a3b3b0f42d45ba9575c7411b
Akira Cai <blackwindforce@users.noreply.github.com>
2024-11-25 01:26:25 +0800

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,15 @@ import prettier from "eslint-config-prettier";
 import typescript from "typescript-eslint";
 
 export default typescript.config(
-  { ignores: ["dist/"] },
+  { ignores: ["dist"] },
   eslint.configs.recommended,
   ...typescript.configs.stylisticTypeChecked,
   prettier,
   {
     languageOptions: {
       parserOptions: {
-        ecmaVersion: 2023,
-        project: true,
+        ecmaVersion: 2025,
+        project: ["tsconfig.json", "tsconfig.eslint.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
