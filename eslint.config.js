6ff3139d42b00ebc2445651b1b54e8b845c471a0
Akira Cai <blackwindforce@users.noreply.github.com>
2025-12-23 00:21:31 +0800

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+import typescript from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  typescript.configs.strictTypeChecked,
+  typescript.configs.stylisticTypeChecked,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2026,
+        projectService: { allowDefaultProject: ["*.js"] },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
